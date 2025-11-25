24d23cf587c794a2209c4c75e545878e41a38d2a
Ilham <ilham.abahoua@icloud.com>
2025-11-25 09:21:36 +0100

    ja

---
+++ functions/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace functions;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Program program = new Program();
+        // variable : program
+        // type : Program
+        
+        program.Run();  
+        // de methode word aangeroepen op de variable program
+    }
+
+
+public void Run()
+    {
+        Console.WriteLine("Hopelijk werkt de functie");
+    }
+}
+
+
+
+
