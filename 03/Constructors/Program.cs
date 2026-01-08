92a99f253ce241b0ab4982628c24be558c62be1f
Ilham <ilham.abahoua@icloud.com>
2026-01-08 15:02:46 +0100

    commit

--- 03/Constructors/Program.cs
+++ 03/Constructors/Program.cs
@@ -10,15 +10,33 @@ namespace Constructors
             p.Run();
         }
 
-        void Run()
-        {
-            QuizVraag quizVraag = new QuizVraag(
-                "hier komt de vraag",
-                "hier komt het antwoord"
-            );
+void Run()
+{
+    Quiz quiz = new Quiz(10);
 
-            Console.WriteLine(quizVraag.vraag);
-            Console.WriteLine(quizVraag.antwoord);
-        }
+    
+    quiz.VoegVraagToeOpIndex(0, "Wat is 2 + 2?", "4");
+    quiz.VoegVraagToeOpIndex(1, "Hoofdstad van Nederland?", "Amsterdam");
+    quiz.VoegVraagToeOpIndex(2, "Kleur van de lucht?", "Blauw");
+    quiz.VoegVraagToeOpIndex(3, "5 + 5?", "10");
+    quiz.VoegVraagToeOpIndex(4, "Dag na maandag?", "Dinsdag");
+
+    
+    QuizVraag v5 = new QuizVraag("1 + 1?", "2");
+    QuizVraag v6 = new QuizVraag("Hoofdstad van Frankrijk?", "Parijs");
+    QuizVraag v7 = new QuizVraag("Kleur van gras?", "Groen");
+    QuizVraag v8 = new QuizVraag("10 / 2?", "5");
+    QuizVraag v9 = new QuizVraag("Zon komt op in het?", "Oosten");
+
+    quiz.VoegVraagToeOpIndex(5, v5);
+    quiz.VoegVraagToeOpIndex(6, v6);
+    quiz.VoegVraagToeOpIndex(7, v7);
+    quiz.VoegVraagToeOpIndex(8, v8);
+    quiz.VoegVraagToeOpIndex(9, v9);
+
+    
+    Console.WriteLine(quiz.vragen[0].vraag);
+    Console.WriteLine(quiz.vragen[9].vraag);
+}
     }
 }
--- 03/Constructors/Quiz.cs
+++ 03/Constructors/Quiz.cs
@@ -4,5 +4,24 @@ namespace Constructors
     {
         internal QuizVraag[] vragen;
         internal QuizVraagAntwoord[] ingevuldeAntwoorden;
+
+        internal Quiz(int aantalVragen)
+        {
+            vragen = new QuizVraag[aantalVragen];
+            ingevuldeAntwoorden = new QuizVraagAntwoord[aantalVragen];
+        }
+
+       
+        internal void VoegVraagToeOpIndex(int index, QuizVraag vraag)
+        {
+            vragen[index] = vraag;
+        }
+
+        
+        internal void VoegVraagToeOpIndex(int index, string vraag, string antwoord)
+        {
+            QuizVraag nieuweVraag = new QuizVraag(vraag, antwoord);
+            VoegVraagToeOpIndex(index, nieuweVraag); // LET OP: andere functie gebruiken
+        }
     }
 }
