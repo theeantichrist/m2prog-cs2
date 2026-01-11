a04325794bfad444896d9c21a4ff003c6930767f
Ilham <ilham.abahoua@icloud.com>
2026-01-11 19:49:48 +0100

    dictionary

---
+++ 05 dictionaries/Dictionaries/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictionaries
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.Run();
+        }
+
+        void Run()
+        {
+            Dictionary<int, string> personeelOpNummer = new Dictionary<int, string>();
+
+            personeelOpNummer.Add(382942, "jim");
+            personeelOpNummer.Add(789432, "tim");
+
+            
+            foreach (KeyValuePair<int, string> item in personeelOpNummer)
+            {
+                Console.WriteLine($"{item.Key}: {item.Value}");
+            }
+        }
+    }
+}
